046c7b778aeef737713785050c53607b2b671d12
Pepe Blasco <pepeblascondc@gmail.com>
2018-04-29 23:28:29 +0200

    primer test lanzamiento actividades

--- app/src/main/java/es/upv/pepelauraeloy/apptelem/MainActivity.java
+++ app/src/main/java/es/upv/pepelauraeloy/apptelem/MainActivity.java
@@ -1,5 +1,6 @@
 package es.upv.pepelauraeloy.apptelem;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        boton = (Button) findViewById(R.id.boton1);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEstadisticas();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void openEstadisticas(){
+        Intent intent = new Intent(this, Estadisticas.class);
+        startActivity(intent);
     }
 
     @Override
