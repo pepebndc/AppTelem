5c6bb24de77be5685ecd80f80aa37a0b3dc8024c
Pepe Blasco <pepeblascondc@gmail.com>
2018-05-03 00:21:01 +0200

    set creditos totales y troncales

--- app/src/main/java/es/upv/pepelauraeloy/apptelem/Plan.java
+++ app/src/main/java/es/upv/pepelauraeloy/apptelem/Plan.java
@@ -1,11 +1,12 @@
 package es.upv.pepelauraeloy.apptelem;
 
-import android.animation.ObjectAnimator;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.Iterator;
 
 public class Plan extends AppCompatActivity {
 
@@ -22,7 +23,7 @@ public class Plan extends AppCompatActivity {
     private TextView percentajeEspecialidad;
     private TextView percentajeOptativo;
 
-    private TextView creditosTroncal;
+    private TextView creditosTroncalText;
     private TextView creditosEspecialidad;
     private TextView creditosOptativos;
     private TextView creditosOptativosAsignatura;
@@ -43,14 +44,14 @@ public class Plan extends AppCompatActivity {
         progressOptativo = (ProgressBar) findViewById(R.id.plan_progress_optativo);
 
         botonAddOptativo = (Button) findViewById(R.id.plan_boton_addCreditos);
-        botonTFG =(Button) findViewById(R.id.plan_tfg_Boton);
+        botonTFG = (Button) findViewById(R.id.plan_tfg_Boton);
 
         percentajeTotal = (TextView) findViewById(R.id.plan_percentaje_total);
         percentajeTroncal = (TextView) findViewById(R.id.plan_percentaje_troncal);
         percentajeEspecialidad = (TextView) findViewById(R.id.plan_porcentaje_especialidad);
         percentajeOptativo = (TextView) findViewById(R.id.plan_percentaje_optativo);
 
-        creditosTroncal = (TextView) findViewById(R.id.plan_creditos_troncal);
+        creditosTroncalText = (TextView) findViewById(R.id.plan_creditos_troncal);
         creditosEspecialidad = (TextView) findViewById(R.id.plan_creditos_especialidad);
         creditosOptativos = (TextView) findViewById(R.id.plan_creditos_optativos_total);
         creditosOptativosAsignatura = (TextView) findViewById(R.id.plan_creditos_optativos_asignatura);
@@ -58,6 +59,95 @@ public class Plan extends AppCompatActivity {
         creditosOptativosExtra = (TextView) findViewById(R.id.plan_creditos_optativos_extra);
 
         estadoTFG = (TextView) findViewById(R.id.plan_tfg_Estado);
+
+        //Hacer cosas
+
+        //Cálculo de creditos superados
+
+        double creditosTotal=0;
+        double creditosTroncal=0;
+        double creditosSistemas=0;
+        double creditosElectronica=0;
+        double creditosTelematica=0;
+        double creditosSonido=0;
+
+        double creditosTotalsuperados=0;
+        double creditosTroncalsuperados=0;
+        double creditosSistemassuperados=0;
+        double creditosElectronicasuperados=0;
+        double creditosTelematicasuperados=0;
+        double creditosSonidosuperados=0;
+
+        Iterator<Asignatura> it = MainActivity.getAppUser().getAsignaturas().iterator();
+        while (it.hasNext()) {
+            Asignatura a = it.next();
+
+            if (a.getRama() == 0) {
+                creditosTroncal = creditosTroncal + a.getCreditos();
+                if(a.getEstado()==2 && a.getCalificacion()>=5){
+                    creditosTroncalsuperados = creditosTroncalsuperados + a.getCreditos();
+                }
+            }
+
+            if (a.getRama() == 1) {
+                creditosSistemas = creditosSistemas + a.getCreditos();
+                if(a.getEstado()==2 && a.getCalificacion()>=5){
+                    creditosSistemassuperados = creditosSistemassuperados + a.getCreditos();
+                }
+            }
+
+            if (a.getRama() == 2) {
+                creditosElectronica = creditosElectronica + a.getCreditos();
+                if(a.getEstado()==2 && a.getCalificacion()>=5){
+                    creditosElectronicasuperados = creditosElectronicasuperados + a.getCreditos();
+                }
+            }
+
+            if (a.getRama() == 3) {
+                creditosSonido = creditosSonido + a.getCreditos();
+                if(a.getEstado()==2 && a.getCalificacion()>=5){
+                    creditosSonidosuperados = creditosSonidosuperados + a.getCreditos();
+                }
+            }
+
+            if (a.getRama() == 4) {
+                creditosTelematica = creditosTelematica + a.getCreditos();
+                if(a.getEstado()==2 && a.getCalificacion()>=5){
+                    creditosTelematicasuperados = creditosTelematicasuperados + a.getCreditos();
+                }
+            }
+
+            if (a.getRama() == 5) {
+                creditosTotal = creditosTotal + a.getCreditos();
+                if(a.getEstado()==2 && a.getCalificacion()>=5){
+                    creditosTotalsuperados = creditosTotalsuperados + a.getCreditos();
+                }
+            }
+
+        }
+
+        //Gestión de créditos
+
+        //Créditos totales
+
+        creditosTotal = creditosTotal + creditosElectronica + creditosSistemas + creditosSonido + creditosTelematica + creditosTroncal;
+        creditosTotalsuperados = creditosTotalsuperados + creditosTelematicasuperados + creditosSonidosuperados + creditosElectronicasuperados + creditosSistemassuperados + creditosTroncalsuperados;
+        System.out.println("Total superados: " + creditosTotalsuperados);
+        double ratioTotal = 100 * creditosTotalsuperados / 240.0;
+        System.out.println("Ratio: " + ratioTotal);
+        progressTotal.setProgress((int)ratioTotal);
+        percentajeTotal.setText(Double.toString(ratioTotal).substring(0,1)+ "%");
+
+
+        //Creditos troncales
+        double ratioTroncal = 100 * creditosTroncalsuperados/creditosTroncal;
+        progressTroncal.setProgress((int)ratioTroncal);
+        percentajeTroncal.setText(Double.toString(ratioTroncal).substring(0,1) + "%");
+        creditosTroncalText.setText("Créditos : "+Double.toString(creditosTroncalsuperados)+"/"+creditosTroncal);
+
+
+
+
 
     }
 
@@ -141,12 +231,12 @@ public class Plan extends AppCompatActivity {
         this.percentajeOptativo = percentajeOptativo;
     }
 
-    public TextView getCreditosTroncal() {
-        return creditosTroncal;
+    public TextView getCreditosTroncalText() {
+        return creditosTroncalText;
     }
 
-    public void setCreditosTroncal(TextView creditosTroncal) {
-        this.creditosTroncal = creditosTroncal;
+    public void setCreditosTroncalText(TextView creditosTroncal) {
+        this.creditosTroncalText = creditosTroncal;
     }
 
     public TextView getCreditosEspecialidad() {
